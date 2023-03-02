8d2a6a1ad0be2ba6d2b8fdfc711aab403e03adcf
jyxop <2000dimitrije@gmail.com>
2023-03-02 09:53:59 +0100

    Napravljena klasa Knjiga

---
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -0,0 +1,56 @@
+package biblioteka;
+
+public class Knjiga {
+
+	private String naslov;
+	
+	private long isbn;
+	
+	private String autori;
+	
+	private String izdavac;
+	
+	private int izdanje;
+
+	public String getNaslov() {
+		return naslov;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public String getAutori() {
+		return autori;
+	}
+
+	public String getIzdavac() {
+		return izdavac;
+	}
+
+	public int getIzdanje() {
+		return izdanje;
+	}
+
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public void setAutori(String autori) {
+		this.autori = autori;
+	}
+
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	
+	
+}
